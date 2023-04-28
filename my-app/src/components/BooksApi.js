dea1bc3f2032d129bb10caeb8635d5765c61d8ca
James Geneser <jamesgeneser@gmail.com>
2023-04-27 20:50:53 -0600

    puzzled out how to conditionally render search results

--- my-app/src/components/BooksApi.js
+++ my-app/src/components/BooksApi.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect, Component } from "react";
 const url = "https://www.googleapis.com/books/v1/volumes?q=";
 const key = "AIzaSyDyUh9tTZjRYDn1uNQbyK8fgrSAGsMKnW4";
-// const searchQuery = "baseball";
+// import Button from "react-bootstrap/Button";
+// import Card from "react-bootstrap/Card";
+
+// <Card style={{ width: '18rem' }}>
+//   <Card.Img variant="top" src="holder.js/100px180" />
+//   <Card.Body>
+//     <Card.Title>Card Title</Card.Title>
+//     <Card.Text>
+//       Some quick example text to build on the card title and make up the
+//       bulk of the card's content.
+//     </Card.Text>
+//     <Button variant="primary">Go somewhere</Button>
+//   </Card.Body>
+// </Card>
 
 function BookSearch(searchQuery) {
   const apiURL = url + searchQuery + "&maxResults=1" + "&key=" + key;
   console.log("books search query" + { searchQuery });
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
   fetch(apiURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // console.log(data);
+      console.log(data);
       const bookDataArr = data.items;
       for (let i = 0; i < 20; i++) {
         console.log(bookDataArr);
-        // let book = bookDataArr[i];
-        // let bookInfo = book.volumeInfo;
-        // let bookTitle = bookInfo.title;
-        // console.log(bookTitle);
+        let book = bookDataArr[i];
+        let bookInfo = book.volumeInfo;
+        let bookTitle = bookInfo.title;
+        console.log(bookTitle);
       }
     });
-
-  useEffect(() => {
-    localStorage.setItem("dataKey", JSON.stringify(data));
-  }, [data]);
 }
+//   useEffect(() => {
+//     localStorage.setItem("dataKey", JSON.stringify(data));
+//   }, [data]);
 
 export default BookSearch;
--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -4,21 +4,11 @@ import ReactDOM from "react-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
-// function getSearchQuery(event) {
-//   event.preventDefault();
-//   const node = document.getElementById("searchQuery");
-//   const searchQuery = ReactDOM.findDOMNode(node).textContent();
-//   console.log(searchQuery);
-// }
-
 const Home = () => {
   const [searchState, setSearchState] = useState({ searchQuery: "" });
-  console.log(searchState);
+  console.log({ searchState } + "search state 9 home js");
 
-  let [searchSent, setSearchSent] = useState({ searchSent: false });
-
-  //   const [bookState, setBookState] = useState({ searchsubmitted: false });
-  //   console.log(bookState + "bookstate");
+  //   console.log(searchQueryRender.searchSent + "search query render home 14");
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -31,62 +21,35 @@ const Home = () => {
   function submitSearch(event) {
     console.log("click");
     event.preventDefault();
-    setSearchSent({ searchSent: true });
-    // const node = document.getElementById("b");
-    // const booksApi = ReactDOM.findDOMNode(node);
-    // console.log(booksApi);
-    // booksApi.setAttribute("searchSubmitted", "true");
-    // setBookState({ searchsubmitted: true });
-    // const searchInput = searchState.searchQuery;
+    // setSearchQueryRender((searchQueryRender.searchSent = true));
+    // console.log(searchQueryRender.searchSent);
 
-    // return searchInput;
+    BookSearch(searchState.searchQuery);
   }
-  if (searchSent == true) {
-    return (
-      <>
-        <h1></h1>
-        <Form onSubmit={submitSearch}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Search</Form.Label>
-            <Form.Control
-              name="searchQuery"
-              type="searchQuery"
-              placeholder="enter search here"
-              value={searchState.searchQuery}
-              onChange={handleChange}
-            />
-          </Form.Group>
 
-          <BookSearch id="b" data={searchState.searchQuery} />
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <h1></h1>
-        <Form onSubmit={submitSearch}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Search</Form.Label>
-            <Form.Control
-              name="searchQuery"
-              type="searchQuery"
-              placeholder="enter search here"
-              value={searchState.searchQuery}
-              onChange={handleChange}
-            />
-          </Form.Group>
+  return (
+    <>
+      {console.log("search sent")}
+      <h1></h1>
+      <Form onSubmit={submitSearch}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Search</Form.Label>
+          <Form.Control
+            name="searchQuery"
+            type="searchQuery"
+            placeholder="enter search here"
+            value={searchState.searchQuery}
+            onChange={handleChange}
+          />
+        </Form.Group>
 
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-      </>
-    );
-  }
+        {/* {BookSearch(searchState.searchQuery)} */}
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default Home;
