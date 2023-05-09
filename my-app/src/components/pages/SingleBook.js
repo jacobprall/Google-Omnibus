da0182e76bcf1ae866aff2cb6a62b18c4336f57d
James Geneser <jamesgeneser@gmail.com>
2023-05-09 13:57:22 -0600

    importing bootstrap card to single book

--- my-app/src/components/pages/SingleBook.js
+++ my-app/src/components/pages/SingleBook.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Card from "react-bootstrap/Card";
 // import getSingleBook from "../../utils/getSingleBook";
 
 const SingleBook = () => {
@@ -80,6 +81,18 @@ const SingleBook = () => {
   return (
     <>
       <h1>Single Book</h1>
+      import Button from 'react-bootstrap/Button';
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
       <Button href="/">Go Home</Button>
     </>
   );
